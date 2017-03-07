b5e2f029980373b0af73a29ee47a4fba5ceafac7
Jinay Patel <jinay@Jinays-MacBook-Air.local>
2017-03-08 01:44:36 +0530

    added canny code.. though incomplete. but backing up.

---
+++ build/CMakeFiles/testApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testApp.dir/main.cpp.o"
+  "CMakeFiles/testApp.dir/medianFilter/median.cpp.o"
+  "CMakeFiles/testApp.dir/cannyEdgeDetector/canny.cpp.o"
+  "CMakeFiles/testApp.dir/thirdparty/gtest/gtest-all.cc.o"
+  "testApp.pdb"
+  "testApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
